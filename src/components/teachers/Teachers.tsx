5f1af8c303f7958a0500380e7446b6057abf1e27
Sergiyan92 <sergiyan@ukr.net>
2023-12-14 16:37:55 +0200

    create style

--- src/components/teachers/Teachers.tsx
+++ src/components/teachers/Teachers.tsx
@@ -3,6 +3,7 @@ import css from "./Teachers.module.css";
 import DropdownList from "../filter/DropdownList";
 import book from "../../assets/book-open-01.svg";
 import star from "../../assets/star.svg";
+
 interface Review {
   reviewer_name: string;
   reviewer_rating: number;
@@ -40,6 +41,9 @@ const Teachers: React.FC = () => {
     value: string;
     label: string;
   } | null>(null);
+  const [expandedTeachers, setExpandedTeachers] = useState<
+    Record<string, boolean>
+  >({});
 
   useEffect(() => {
     const fetchTeachers = async () => {
@@ -99,9 +103,16 @@ const Teachers: React.FC = () => {
   const handleLoadMore = () => {
     setTeachersToShow((prev) => prev + 4);
   };
+
+  const handleReadMoreToggle = (teacherId: string) => {
+    setExpandedTeachers((prev) => ({
+      ...prev,
+      [teacherId]: !prev[teacherId],
+    }));
+  };
+
   return (
-    <section>
-      <h2>Teachers</h2>
+    <section className={css.section}>
       <div className={css.filter}>
         <DropdownList
           options={languageOptions}
@@ -131,43 +142,104 @@ const Teachers: React.FC = () => {
             </div>
             <div>
               <div className={css.info}>
-                <p>Languages</p>
-                <img src={book} alt="book icon" /> <span>Lessons online</span> |{" "}
-                <span>Lessons done: {teacher.lessons_done}</span>|{" "}
-                <img src={star} alt="star icon" />
-                <span>Rating: {teacher.rating}</span>
+                <p className={css.title}>Languages</p>
+                <img className={css.icon} src={book} alt="book icon" />
+                <span className={css.subtitle}>Lessons online</span> |
+                <span className={css.subtitle}>
+                  Lessons done: {teacher.lessons_done}
+                </span>
+                | <img className={css.icon} src={star} alt="star icon" />
+                <span className={css.subtitle}>Rating: {teacher.rating}</span> |
+                <span className={css.subtitle}>
+                  Price 1 / hour:
+                  <span className={css.price}> {teacher.price_per_hour}$</span>
+                </span>
               </div>
-            </div>
-            {/* <div>
-              {teacher.name} {teacher.surname}
-            </div>
-            <div>Languages: {teacher.languages.join(", ")}</div>
-            <div>Levels: {teacher.levels.join(", ")}</div>
-            <div>Rating: {teacher.rating}</div>
-            <div>
-              Reviews:
-              <ul>
-                {teacher.reviews.map((review, reviewIndex) => (
-                  <li key={reviewIndex}>
-                    <div>
-                      {review.reviewer_name} ({review.reviewer_rating}):{" "}
-                      {review.comment}
-                    </div>
+              <h2 className={css.name}>
+                {teacher.name} {teacher.surname}
+              </h2>
+              <div className={css.info_leng}>
+                <span className={css.title_sub}>Speaks:</span>
+                <span className={css.leng}>
+                  {" "}
+                  {teacher.languages.join(", ")}
+                </span>
+              </div>
+              <div className={css.lesson}>
+                <span className={css.title_sub}>Lesson Info:</span>
+                <span className={css.title_lesson}>
+                  {" "}
+                  {teacher.lesson_info}
+                </span>{" "}
+              </div>
+              <div className={css.lesson}>
+                <span className={css.title_sub}>Conditions: </span>
+                <span className={css.title_lesson}>
+                  {teacher.conditions.join(" ")}
+                </span>
+                <div>
+                  {expandedTeachers[teacher.name + teacher.surname] ? (
+                    <>
+                      <ul>
+                        <div className={css.exp}>{teacher.experience}</div>
+                        {teacher.reviews.map((review, reviewIndex) => (
+                          <li key={reviewIndex}>
+                            <div className={css.info_coment}>
+                              <span className={css.title_sub}>
+                                {review.reviewer_name}{" "}
+                              </span>
+                              <div className={css.raiting}>
+                                <img
+                                  className={css.icon_raiting}
+                                  src={star}
+                                  alt="star icon"
+                                />
+                                {review.reviewer_rating}
+                              </div>
+                              <span>{review.comment}</span>
+                            </div>
+                          </li>
+                        ))}
+                      </ul>
+                      <button
+                        className={css.btn_read}
+                        onClick={() =>
+                          handleReadMoreToggle(teacher.name + teacher.surname)
+                        }
+                      >
+                        Read Less
+                      </button>
+                    </>
+                  ) : (
+                    <button
+                      className={css.btn_read}
+                      onClick={() =>
+                        handleReadMoreToggle(teacher.name + teacher.surname)
+                      }
+                    >
+                      Read More
+                    </button>
+                  )}
+                </div>
+              </div>
+              <div className={css.level}>
+                {teacher.levels.map((level, levelindex) => (
+                  <li key={levelindex} className={css.level_leng}>
+                    #{level}
                   </li>
                 ))}
-              </ul>
+              </div>
             </div>
-            <div>Price per hour: {teacher.price_per_hour}</div>
-            <div>Lessons done: {teacher.lessons_done}</div>
-            <div>Lesson Info: {teacher.lesson_info}</div>
-            <div>Conditions: {teacher.conditions.join(", ")}</div>
-            <div>Experience: {teacher.experience}</div> */}
           </li>
         ))}
       </ul>
-      {teachers.length > displayedTeachers.length && (
-        <button onClick={handleLoadMore}>Load More</button>
-      )}
+      <div className={css.btn}>
+        {teachers.length > displayedTeachers.length && (
+          <button className={css.btn_load} onClick={handleLoadMore}>
+            Load More
+          </button>
+        )}
+      </div>
     </section>
   );
 };
