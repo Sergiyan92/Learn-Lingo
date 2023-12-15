38b9fcaeee13952cf12d1f4da55b0156238a4a3c
Sergiyan92 <sergiyan@ukr.net>
2023-12-15 08:48:49 +0200

    add header on Teachers page

--- src/pages/TeachersPage.tsx
+++ src/pages/TeachersPage.tsx
@@ -1,10 +1,12 @@
 // import Card from "../components/card/Card";
+import Header from "../components/header/Header";
 import Teachers from "../components/teachers/Teachers";
 // import DropdownList from "../components/filter/DropdownList";
 
 const TeachersPage = () => {
   return (
     <div>
+      <Header />
       <Teachers />
       {/* <Card /> */}
     </div>
