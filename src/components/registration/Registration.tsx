843b5fb5cddbeb726093d82207b388e816f2b73d
Sergiyan92 <sergiyan@ukr.net>
2023-12-19 10:28:25 +0200

    create style register form

--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -15,7 +15,6 @@ const Registration = ({ onClose }: Modal) => {
     },
     validationSchema: Yup.object({
       firstName: Yup.string().required("Required"),
-      lastName: Yup.string().required("Required"),
       email: Yup.string().email("Invalid email address").required("Required"),
       password: Yup.string()
         .min(6, "Must be at least 6 characters")
@@ -30,24 +29,25 @@ const Registration = ({ onClose }: Modal) => {
   });
   return (
     <form onSubmit={formik.handleSubmit} className={css.reg}>
+      <button className={css.btn_close} onClick={() => onClose()}>
+        <img src={close} alt="icon close" width={32} height={32} />
+      </button>
       <div>
-        <h2>Registration</h2>
-        <button onClick={() => onClose()}>
-          <img src={close} alt="icon close" width={32} height={32} />
-        </button>
-        <p>
+        <h2 className={css.title_reg}>Registration</h2>
+        <p className={css.sub_title_reg}>
           Thank you for your interest in our platform! In order to register, we
           need some information. Please provide us with the following
           information
         </p>
-        <label htmlFor="firstName">First Name</label>
         <input
           type="text"
           id="firstName"
           name="firstName"
+          placeholder="Name"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.firstName}
+          className={css.input_name}
         />
         {formik.touched.firstName && formik.errors.firstName ? (
           <div>{formik.errors.firstName}</div>
@@ -55,14 +55,15 @@ const Registration = ({ onClose }: Modal) => {
       </div>
 
       <div>
-        <label htmlFor="email">Email Address</label>
         <input
           type="email"
           id="email"
           name="email"
+          placeholder="Email"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.email}
+          className={css.input}
         />
         {formik.touched.email && formik.errors.email ? (
           <div>{formik.errors.email}</div>
@@ -70,22 +71,25 @@ const Registration = ({ onClose }: Modal) => {
       </div>
 
       <div>
-        <label htmlFor="password">Password</label>
         <input
           type="password"
           id="password"
           name="password"
+          placeholder="Password"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.password}
+          className={css.input}
         />
         {formik.touched.password && formik.errors.password ? (
           <div>{formik.errors.password}</div>
         ) : null}
       </div>
 
-      <div>
-        <button type="submit">Register</button>
+      <div className={css.btn}>
+        <button className={css.btn_reg} type="submit">
+          <span className={css.btn_text}>Register</span>
+        </button>
       </div>
     </form>
   );
